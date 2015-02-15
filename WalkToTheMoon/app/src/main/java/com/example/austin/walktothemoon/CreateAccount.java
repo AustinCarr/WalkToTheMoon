528df4c53fbc1965de80e65c4049b8312c20d780
Austin Carr <austin.r.carr@gmail.com>
2015-02-15 17:13:51 -0500

    Skeleton of UI
    Creates skeleton classes of UI

---
+++ WalkToTheMoon/app/src/main/java/com/example/austin/walktothemoon/CreateAccount.java
@@ -0,0 +1,52 @@
+package com.example.austin.walktothemoon;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class CreateAccount extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_create_account, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onNextClicked(View v) {
+        Intent intent = new Intent(this, ChooseProfilePic.class);
+        startActivity(intent);
+
+    }
+}
---
+++ WalkToTheMoon/app/src/main/java/com/example/austin/walktothemoon/Profile.java
@@ -0,0 +1,21 @@
+package com.example.austin.walktothemoon;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Profile extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+}
