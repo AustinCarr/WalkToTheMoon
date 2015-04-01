8ee81eff56cbf27c59c2a82bd66b8a07d914f8a9
Mariya Medovar <medovamx@dukes.jmu.edu>
2015-04-01 14:34:45 -0400

    Fixed winning page

--- WalkToTheMoon/app/src/main/java/com/example/austin/walktothemoon/WinningActivity.java
+++ WalkToTheMoon/app/src/main/java/com/example/austin/walktothemoon/WinningActivity.java
@@ -1,18 +1,27 @@
 package com.example.austin.walktothemoon;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class WinningActivity extends Activity {
 
+    TextView message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winning);
+
+        message = (TextView) findViewById(R.id.text_view_congrats);
+
+        Typeface tobiBlack;
+        tobiBlack = Typeface.createFromAsset(getAssets(), "fonts/TobiBlack.otf");
+        message.setTypeface(tobiBlack);
     }
 
 
