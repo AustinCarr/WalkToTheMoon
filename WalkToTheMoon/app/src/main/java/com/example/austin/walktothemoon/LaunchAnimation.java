6d9100c4652caa3ac0a32698068c4c1e1195c879
Austin Carr <austin.r.carr@gmail.com>
2015-04-08 14:58:41 -0400

    Adds Astronaut Images
    Fixes Springs on Create Profile image.
    Adds new speech bubble image on Choose Profile Pic

--- WalkToTheMoon/app/src/main/java/com/example/austin/walktothemoon/LaunchAnimation.java
+++ WalkToTheMoon/app/src/main/java/com/example/austin/walktothemoon/LaunchAnimation.java
@@ -29,10 +29,10 @@ public class LaunchAnimation extends Activity {
 
             public void run() {
 
-                /*Intent intent = new Intent(LaunchAnimation.this, MainActivity.class);
+                Intent intent = new Intent(LaunchAnimation.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
-                finish();*/
+                finish();
 
             }
 
